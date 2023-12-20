149da8b448a583afed4e9f5f92242a3a1c628601
kelllyoliveira <kellyoliveira@gmail.com>
2023-12-20 17:04:03 -0300

    Atualizações das operações do TransactionService/TransactionDTO

--- WiseControl.Application/Services/TransactionService.cs
+++ WiseControl.Application/Services/TransactionService.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using WiseControl.Domain.Entities;
+using WiseControl.Domain.Interfaces;
+using WiseControl.DTOs;
 
 namespace WiseControl.Application.Services
 {
-    internal class TransactionService
+    public class TransactionService : ITransactionService
     {
+
+        private ITransactionRepository _transactionRepository;
+        private readonly IMapper _mapper;
+
+        public TransactionService(ITransactionRepository transactionRepository, IMapper mapper)
+        {
+            _transactionRepository = transactionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<TransactionDTO>> GetTransactions()
+        {
+            var transactions= await _transactionRepository.GetTransactionsAsync();
+            return _mapper.Map<IEnumerable<TransactionDTO>>(transactions);
+        }
+
+        public async Task<TransactionDTO> GetById(int? id)
+        {
+            var transactionEntity = await _transactionRepository.GetByIdAsync(id);
+            return _mapper.Map<TransactionDTO>(transactionEntity);
+        }
+
+        public async Task Add(TransactionDTO transactionDto)
+        {
+            var transactionEntity = _mapper.Map<Transaction>(transactionDto);
+            await _transactionRepository.CreateAsync(transactionEntity);
+        }
+
+        public async Task Update(TransactionDTO transactionDto)
+        {
+            var transactionEntity = _mapper.Map<Transaction>(transactionDto);
+            await _transactionRepository.UpdateAsync(transactionEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var transactionEntity = _transactionRepository.GetByIdAsync(id).Result;
+            await _transactionRepository.RemoveAsync(transactionEntity);
+        }
+
+
     }
 }
--- WiseControl.DTOs/TransactionDTO.cs
+++ WiseControl.DTOs/TransactionDTO.cs
@@ -3,5 +3,18 @@
     public class TransactionDTO
     {
         public int Id { get; set; }
+
+
+        public TransactionType Type { get; set; }
+        public String Description { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Value { get; set; }
+
+
+        public enum TransactionType
+        {
+            Income,
+            Expense
+        }
     }
 }
